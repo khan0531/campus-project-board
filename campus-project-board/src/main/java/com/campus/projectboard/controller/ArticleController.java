a2eeddfd6f9b0e3a4553d117e1fe7d635e83b843
han <rudgksdl94@gmail.com>
2024-04-28 23:08:33 +0900

    #16 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰 적용

--- campus-project-board/src/main/java/com/campus/projectboard/controller/ArticleController.java
+++ campus-project-board/src/main/java/com/campus/projectboard/controller/ArticleController.java
@@ -8,6 +8,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,5 +21,14 @@ public class ArticleController {
     map.addAttribute("articles", List.of());
 
     return "articles/index";
+  }
+
+  @GetMapping("/{articleId}")
+  public String article(@PathVariable Long articleId, ModelMap map) {
+
+    map.addAttribute("article", null);
+    map.addAttribute("articleComments", List.of());
+
+    return "articles/detail";
   }
 }
