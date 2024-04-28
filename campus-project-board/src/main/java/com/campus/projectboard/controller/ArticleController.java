87f1a6897875d826bcb5a6b46a31788dc784f80a
han <rudgksdl94@gmail.com>
2024-04-28 09:37:52 +0900

    #16 - 게시판 뷰 기본 골격 작성
    뷰 뼈대 작성
    컨트롤러 기본 메소드 작성

--- campus-project-board/src/main/java/com/campus/projectboard/controller/ArticleController.java
+++ campus-project-board/src/main/java/com/campus/projectboard/controller/ArticleController.java
@@ -1,10 +1,24 @@
 package com.campus.projectboard.controller;
 
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+  @GetMapping
+  public String articles(ModelMap map) {
 
+    map.addAttribute("articles", List.of());
+
+    return "articles/index";
+  }
 }
