ef6ccf4ceef9bfec75bc006f54a484e4d0e85c48
han <rudgksdl94@gmail.com>
2024-04-28 09:05:17 +0900

    #16 - 게시글 관련 뷰 테스트 작성

---
+++ campus-project-board/src/test/java/com/campus/projectboard/controller/ArticleControllerTest.java
@@ -0,0 +1,49 @@
+package com.campus.projectboard.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+  private final MockMvc mvc;
+
+  public ArticleControllerTest(MockMvc mvc) {
+    this.mvc = mvc;
+  }
+
+  @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
+  @Test
+  void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+    // Given
+
+    // When & Then
+    mvc.perform(get("/articles"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+        .andExpect(model().attributeExists("articles"));
+  }
+
+
+
+}
