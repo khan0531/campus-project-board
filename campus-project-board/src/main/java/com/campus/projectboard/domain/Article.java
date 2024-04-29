55da032251babdab51596e7be1503907f7b45b53
han <rudgksdl94@gmail.com>
2024-04-29 13:42:38 +0900

    Article 도메인, dto

--- campus-project-board/src/main/java/com/campus/projectboard/domain/Article.java
+++ campus-project-board/src/main/java/com/campus/projectboard/domain/Article.java
@@ -1,77 +1,99 @@
 package com.campus.projectboard.domain;
 
 import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.*;
-import java.util.LinkedHashSet;
-import java.util.Objects;
-import java.util.Set;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-
 @Getter
 @ToString(callSuper = true)
 @Table(indexes = {
-    @Index(columnList = "title"),
-    @Index(columnList = "createdAt"),
-    @Index(columnList = "createdBy")
+        @Index(columnList = "title"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
 })
 @Entity
 public class Article extends AuditingFields {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Setter
-  @JoinColumn(name = "userId")
-  @ManyToOne(optional = false)
-  private UserAccount userAccount; // 유저 정보 (ID)
+    @Setter
+    @JoinColumn(name = "userId")
+    @ManyToOne(optional = false)
+    private UserAccount userAccount; // 유저 정보 (ID)
 
-  @Setter @Column(nullable = false) private String title; // 제목
-  @Setter @Column(nullable = false, length = 10000) private String content; // 본문
+    @Setter @Column(nullable = false) private String title; // 제목
+    @Setter @Column(nullable = false, length = 10000) private String content; // 본문
 
-//  @ToString.Exclude
-//  @JoinTable(
-//      name = "article_hashtag",
-//      joinColumns = @JoinColumn(name = "articleId"),
-//      inverseJoinColumns = @JoinColumn(name = "hashtagId")
-//  )
-//  @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-//  private Set<Hashtag> hashtags = new LinkedHashSet<>();
-
-
-  @ToString.Exclude
-  @OrderBy("createdAt DESC")
-  @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
-  private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+    @ToString.Exclude
+    @JoinTable(
+            name = "article_hashtag",
+            joinColumns = @JoinColumn(name = "articleId"),
+            inverseJoinColumns = @JoinColumn(name = "hashtagId")
+    )
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private Set<Hashtag> hashtags = new LinkedHashSet<>();
 
 
-  protected Article() {}
+    @ToString.Exclude
+    @OrderBy("createdAt DESC")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
-  private Article(UserAccount userAccount, String title, String content) {
-    this.userAccount = userAccount;
-    this.title = title;
-    this.content = content;
-  }
 
-  public static Article of(UserAccount userAccount, String title, String content) {
-    return new Article(userAccount, title, content);
-  }
+    protected Article() {}
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (!(o instanceof Article that)) return false;
-    return this.getId() != null && this.getId().equals(that.getId());
-  }
+    private Article(UserAccount userAccount, String title, String content) {
+        this.userAccount = userAccount;
+        this.title = title;
+        this.content = content;
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(this.getId());
-  }
+    public static Article of(UserAccount userAccount, String title, String content) {
+        return new Article(userAccount, title, content);
+    }
+
+    public void addHashtag(Hashtag hashtag) {
+        this.getHashtags().add(hashtag);
+    }
+
+    public void addHashtags(Collection<Hashtag> hashtags) {
+        this.getHashtags().addAll(hashtags);
+    }
+
+    public void clearHashtags() {
+        this.getHashtags().clear();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article that)) return false;
+        return this.getId() != null && this.getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId());
+    }
 
 }
