b33bb1c53d9ab0e7a6c3743f45b2f723d91efee8
han <rudgksdl94@gmail.com>
2024-05-01 20:31:35 +0900

    #25 - 게시글 댓글 컨트롤러, 서비스

---
+++ campus-project-board/src/main/java/com/campus/projectboard/controller/ArticleCommentController.java
@@ -0,0 +1,21 @@
+package com.campus.projectboard.controller;
+
+import com.campus.projectboard.dto.request.ArticleCommentRequest;
+import com.campus.projectboard.service.ArticleCommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/comments")
+@Controller
+public class ArticleCommentController {
+
+    private final ArticleCommentService articleCommentService;
+
+
+
+}
--- campus-project-board/src/main/java/com/campus/projectboard/service/ArticleCommentService.java
+++ campus-project-board/src/main/java/com/campus/projectboard/service/ArticleCommentService.java
@@ -1,5 +1,14 @@
 package com.campus.projectboard.service;
 
+import com.campus.projectboard.domain.Article;
+import com.campus.projectboard.domain.ArticleComment;
+import com.campus.projectboard.domain.UserAccount;
+import com.campus.projectboard.dto.ArticleCommentDto;
+import com.campus.projectboard.repository.ArticleCommentRepository;
+import com.campus.projectboard.repository.ArticleRepository;
+import com.campus.projectboard.repository.UserAccountRepository;
+import java.util.List;
+import javax.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,4 +20,32 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class ArticleCommentService {
 
+  private final ArticleRepository articleRepository;
+  private final ArticleCommentRepository articleCommentRepository;
+  private final UserAccountRepository userAccountRepository;
+
+  @Transactional(readOnly = true)
+  public List<ArticleCommentDto> searchArticleComments(Long articleId) {
+    return articleCommentRepository.findByArticle_Id(articleId)
+        .stream()
+        .map(ArticleCommentDto::from)
+        .toList();
+  }
+
+  public void saveArticleComment(ArticleCommentDto dto) {
+    try {
+      Article article = articleRepository.getReferenceById(dto.articleId());
+      UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
+      ArticleComment articleComment = dto.toEntity(article, userAccount);
+
+      if (dto.parentCommentId() != null) {
+        ArticleComment parentComment = articleCommentRepository.getReferenceById(dto.parentCommentId());
+        parentComment.addChildComment(articleComment);
+      } else {
+        articleCommentRepository.save(articleComment);
+      }
+    } catch (EntityNotFoundException e) {
+      log.warn("댓글 저장 실패. 댓글 작성에 필요한 정보를 찾을 수 없습니다 - {}", e.getLocalizedMessage());
+    }
+  }
 }
