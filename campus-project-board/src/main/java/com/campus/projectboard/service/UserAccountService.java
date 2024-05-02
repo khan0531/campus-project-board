1ff0c0f6d37df5a886051848a46f381bab7f1c69
han <rudgksdl94@gmail.com>
2024-05-02 20:41:24 +0900

    #26 - 유저 인증 서비스 코드

--- campus-project-board/src/main/java/com/campus/projectboard/service/UserAccountService.java
+++ campus-project-board/src/main/java/com/campus/projectboard/service/UserAccountService.java
@@ -15,4 +15,16 @@ public class UserAccountService {
 
     private final UserAccountRepository userAccountRepository;
 
+    @Transactional(readOnly = true)
+    public Optional<UserAccountDto> searchUser(String username) {
+        return userAccountRepository.findById(username)
+            .map(UserAccountDto::from);
+    }
+
+    public UserAccountDto saveUser(String username, String password, String email, String nickname, String memo) {
+        return UserAccountDto.from(
+            userAccountRepository.save(UserAccount.of(username, password, email, nickname, memo, username))
+        );
+    }
+
 }
